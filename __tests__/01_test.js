6aed799dc85ed0f511cf26bb7aecc9da40cfcc42
Nick Milon <nickmilon@gmail.com>
2021-02-03 19:39:56 +0200

    0.2.1

--- __tests__/01_test.js
+++ __tests__/01_test.js
@@ -1,12 +1,20 @@
+/* eslint-disable no-new */
 /* eslint-disable no-return-assign */
 /* eslint-disable no-undef */
 /**
  * @jest-environment node
  */
-import { Chronos, Homer, Plato, Solon,  Pythagoras } from '../index.js';
 
-describe('check class', () => {
+import { Chronos, Homer, Plato, Solon,  Pythagoras, Thales } from '../index.js'; // import all to check imports
+
+const { EnumBits } = Thales;
+const logger = (__inspect__ === true) ? console : Plato.DummyLogger;
+// eslint-disable-next-line no-console
+console.info('*** set __inspect__ variable in package.json to true || false to view/hide test details ***');
+
+describe('check Acropolis-nd', () => {
   beforeAll(async () => {
+
   });
 
   afterAll(async () => {
@@ -20,8 +28,36 @@ describe('check class', () => {
     expect(Pythagoras.percent(1, 200)).toEqual(0.5);
     expect(Pythagoras.isNumber('fff')).toEqual(false);
     expect(Pythagoras.isNumber(0.1821)).toEqual(true);
+    expect(Pythagoras.isInRange(-1, -10, 100)).toBe(true);
+    expect(Pythagoras.inRangeOrDefault(2, 0, 1, 3)).toEqual(2);
+    expect(Pythagoras.inRangeOrDefault('b', 'default', 'a', 'c')).toEqual('b');
+    expect(Pythagoras.inRangeOrDefault('b', 'default', 'c', 'd')).toEqual('default');
+    expect(Pythagoras.intOrDefault(-1, 50, -10, 100)).toEqual(-1);
+    expect(Pythagoras.intOrDefault(-1, 50, 1, 100)).toEqual(50);
+    expect(Pythagoras.intOrDefault('-1', 50, -10, 100)).toEqual(-1);
   });
   it('Chronos', async () => {
     expect(Chronos.dtToStrCompressed(new Date('2021-01-30T18:29:33'))).toEqual('210130162933');
+    expect(Chronos.convertMS(100000000)).toEqual('01:03:46:40');
   });
+  it('Thales', async () => {
+    let fruits = new EnumBits([...Array(29).keys()]);
+    await Plato.timeIt(fruits.flagsFromIntArrScan(7), 'flagsFromIntArrScan', 100000, logger);
+    await Plato.timeIt(fruits.flagsFromIntArrLookUp(7), 'flagsFromIntArrLookUp', 100000, logger);
+    // console.log('xxxxxxxxxxxxxxxxxxxx', {result});
+    fruits = new EnumBits(['lemon', 'orange', 'watermelon']);
+    expect(fruits.flagsFromIntObj(6).orange).toEqual(true); // >> { lemon: true, orange: true, watermelon: false }
+    expect(() => { new EnumBits(['lemon', 'lemon']); }).toThrow('EnumBits: flags must be unique but are not');
+    expect(() => { new EnumBits([...Array(53).keys()]); }).toThrow();
+    expect(() => { new EnumBits([...Array(31).keys()], { allow52bits: false }); }).toThrow();
+    expect(() => { new EnumBits(Array.from({ length: 36 }, (_, i) => i + 1), { allow52bits: true }); }).not.toThrow();
+  });
+
+  it('Pythagoras', async () => {
+    expect(Pythagoras.isInRange(100, 99, 100)).toBe(true);
+    expect(Pythagoras.isInRange(-1, 0, 10)).toBe(false);
+  });
+
 });
+
+
--- index.js
+++ index.js
@@ -3,3 +3,5 @@ export * as Chronos from './lib/Chronos.js';
 export * as Homer from './lib/Homer.js';
 export * as Plato from './lib/Plato.js';
 export * as Solon from './lib/Solon.js';
+export * as Thales from './lib/Thales.js';
+export * as Hamartia from './lib/Hamartia.js';
